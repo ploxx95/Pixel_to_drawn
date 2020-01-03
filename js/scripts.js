52502070d26d1a5039e7e68adbaee7d2ce07fbe9
ploxx95 <ploxx95@gmail.com>
2020-01-03 01:22:31 -0500

    pixel black recorrer mouse

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,31 @@ muestra el elemento    document.body.appendChild(divpadre)
 */
 function divson(){
 
+    for(let i=0; i < 256;i++){
+
+    
     let div = document.createElement("div")
     div.setAttribute("class","divhijo")
-    document.getElementById("container").appendChild(div)
-
+    
+    document.getElementsByClassName("containerfin")[0].appendChild(div)
+    }
 }
 
 function creadiv(){
 
     let divpadre = document.createElement("div")
-    divpadre.setAttribute("id","container")
+    divpadre.setAttribute("class","containerfin")
     document.body.appendChild(divpadre)
 
 
  }
+
+
  
  let boton = document.getElementById("button")
   boton.addEventListener('click',creadiv)
   let botonn = document.getElementById("buttonn")
   botonn.addEventListener('click',divson)
- 
+
 
 
